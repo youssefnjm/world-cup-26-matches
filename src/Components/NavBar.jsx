362c0b007d338bd2a426665316e3a007964505c4
youssefnjm <youssefnoujoum04@gmail.com>
2026-06-13 14:51:04 +0100

    depoy new V

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,12 +9,6 @@ const NavBare = () => {
                 <Link to="/">Home</Link>
                 <Link to="/Schedule">Schedule</Link>
                 <Link to="/Groups">Groups</Link>
-                {/* <li><a href="#news">News</a></li>
-                <li><a href="#schedule">Schedule</a></li>
-                <li><a href="#groups">Groups</a></li>
-                <li><a href="#stats">Stats</a></li>
-                <li><a href="#hosts">Venues</a></li> */}
-                {/* <li><a href="#" className="nav-cta">Get Tickets</a></li> */}
             </ul>
         </nav>
     );
